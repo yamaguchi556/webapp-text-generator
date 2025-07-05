f7fdd8552bbea600ec033fc922ffd7575fed11c3
yamaguchi556 <sdgfc.info@gmail.com>
2025-07-05 15:36:14 +0900

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,11 +4,11 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
+  title: '教材作成アプリ',
   description: 'App description',
-  copyright: '',
+  copyright: 'YAMAGUCHI Masatoshi',
   privacy_policy: '',
-  default_language: 'en-US',
+  default_language: 'ja-JP',
 }
 
 export const API_PREFIX = '/api'
